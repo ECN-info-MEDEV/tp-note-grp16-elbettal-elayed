3b88e6f67273ba74fa1c3fec1df2ba9dcccb6a6a
samarelayed <elayedsamar@gmail.com>
2023-01-26 11:19:26 +0100

    test1

---
+++ src/main/java/com/mycompany/tp/note/medev/Joueur.java
@@ -0,0 +1,33 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.tp.note.medev;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author samar
+ */
+public abstract class Joueur {
+    private int score;
+    private ArrayList<Pion> ListPion = new ArrayList<>();
+    public int getScore() {
+        return score;
+    }
+
+    public void setScore(int score) {
+        this.score = score;
+    }
+
+    public ArrayList<Pion> getListPion() {
+        return ListPion;
+    }
+
+    public void setListPion(ArrayList<Pion> ListPion) {
+        this.ListPion = ListPion;
+    }
+    
+    
+}
